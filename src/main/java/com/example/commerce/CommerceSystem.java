b786838dc561d16df9b3d90c07529f9508f7af74
“grown-tree” <tjsruddks12@naver.com>
2026-01-20 21:11:28 +0900

    step2 구현 : Product리스트 필드에 선언, start함수 생성하여 step1 main기능 부여, Main에서 CommerceSystem 객체를 생성 후 CommerceSystem 클래스 생성자를 통해 상품정보 넘겨줌

--- src/main/java/com/example/commerce/CommerceSystem.java
+++ src/main/java/com/example/commerce/CommerceSystem.java
@@ -1,4 +1,57 @@
 package com.example.commerce;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CommerceSystem {
+
+    private int menu = 1;
+
+    private List<Product> products = new ArrayList<>();
+
+    public CommerceSystem() {
+    }
+
+    public CommerceSystem(List<Product> products) {
+        this.products = products;
+    }
+    // List<Product>에 값 할당해주는 생성자
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public void start (){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("start 함수 시작");
+
+        while (menu!=0) {
+            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+            for (int i = 1; i < products.size()+1; i++) {
+                System.out.println(i + ". " + products.get(i-1).getProdInfo());
+            }
+            System.out.println("0. 종료            | 프로그램 종료");
+            menu = sc.nextInt();
+        }
+    }
+
+
+    public int getMenu() {
+        return menu;
+    }
+
+    public void setMenu(int menu) {
+        this.menu = menu;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
 }
--- src/main/java/com/example/commerce/Main.java
+++ src/main/java/com/example/commerce/Main.java
@@ -2,29 +2,27 @@ package com.example.commerce;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int menu = 1;
+        CommerceSystem commerceSystem = new CommerceSystem();
 
-        List<Product> products = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
+//        List<Product> productList = commerceSystem.getProducts();
 
-        products.add(new Product("Galaxy S25", 1200000,"최신 안드로이드 스마트폰", 80));
-        products.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 120));
-        products.add(new Product("MacBook Pro", 2400000,  "M3 칩셋이 탑재된 노트북", 50));
-        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 200));
+//        productList.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 80));
+//        productList.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 120));
+//        productList.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 50));
+//        productList.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 200));
 
-        while (menu!=0) {
-            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-            for (int i = 1; i < products.size()+1; i++) {
-                System.out.println(i + ". " + products.get(i-1).getProdInfo());
-            }
-            System.out.println("0. 종료            | 프로그램 종료");
-            menu = sc.nextInt();
-        }
+        commerceSystem.addProduct(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 80));
+        commerceSystem.addProduct(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 120));
+        commerceSystem.addProduct(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 50));
+        commerceSystem.addProduct(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 200));
+
+
+        commerceSystem.start();
+
 
     }
 }
